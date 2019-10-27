ddf2f11511966e14b1a639e627d76581f75941a7
zhu <819508408@qq.com>
2019-10-27 18:37:54 +0800

    首页轮播图完成

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -2,14 +2,8 @@
   <div class="wrapper">
     <swiper :options="swiperOption">
       <!-- slides -->
-      <swiper-slide>
-        <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/e09bcf3ce7fda2b091b8cc8de80dceca.jpg_750x200_c144ffc3.jpg"/>
-      </swiper-slide>
-      <swiper-slide><!--375*100-->
-        <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/78b240572657a6da67e662540c846dd6.jpg_750x200_b75c8be9.jpg"/>
-      </swiper-slide>
-      <swiper-slide>
-        <img class="swiper-img" src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/8254351bdfba7389f5a4de4416b9d4f4.png_750x200_843e98ad.png"/>
+      <swiper-slide v-for="(item , index) of swiperList" :key="index">
+        <img class="swiper-img" :src="item.imaUrl"/>
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div><!--实际是swiper组件里的内容，不是当前组件-->
     </swiper>
@@ -22,8 +16,22 @@ export default {
   data: function () {
     return {
       swiperOption: {
-        pagination: '.swiper-pagination'
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imaUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/e09bcf3ce7fda2b091b8cc8de80dceca.jpg_750x200_c144ffc3.jpg'
+      },
+      {
+        id: '0002',
+        imaUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/201910/78b240572657a6da67e662540c846dd6.jpg_750x200_b75c8be9.jpg'
+      },
+      {
+        id: '0003',
+        imaUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/8254351bdfba7389f5a4de4416b9d4f4.png_750x200_843e98ad.png'
       }
+      ]
     }
   }
 }
