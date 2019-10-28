395e25fcb7e3bfb424cb6ac9c6c3618217f94785
zhu <819508408@qq.com>
2019-10-28 21:43:47 +0800

    首页部分内容

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+          <img class="item-img" :src="item.imgUrl"/>
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+      </li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1603/b1/b1da46b5326eb99e90.water.jpg_200x200_2f0a2f3f.jpg',
+        title: '武汉欢乐谷',
+        desc: '来试试中国最高的弹射过山车吧~'
+      },
+      {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1501/10/1012e4a2c398409c.water.jpg_200x200_366b12f8.jpg',
+        title: '云雾山',
+        desc: '来看漂亮的杜鹃花~'
+      },
+      {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201405/27/11a29c646d897af77648bc3d98949107.jpg_200x200_0b6014b8.jpg',
+        title: '武汉九峰山森林动物园',
+        desc: '与动物亲近，尽享自然风光秀色'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height:.8rem
+    background: #eee
+    text-indent: .2rem
+    text-align: left
+  .item
+    overflow:hidden
+    display:flex
+    height:1.9rem
+    text-align:left
+    .item-img
+      width:1.7rem
+      height:1.7rem
+      padding:.1rem
+    .item-info
+      flex:1
+      padding:.1rem
+      min-width:0
+      .item-title
+        line-height:.54rem
+        font-size:.32rem
+        ellipsis()
+      .item-desc
+        line-height:.4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height:.44rem
+        margin-top:.16rem
+        background:#ff9300
+        padding:0 .2rem
+        border-radius:.06rem
+        color:#fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="recommend-title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl"/>
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1509/84/96e0a5830d7c99.jpg_r_640x214_ee164725.jpg',
+        title: '江城山水',
+        desc: '北枕巍巍大别山，南瀕浩浩长江水，江城内外，还隐藏的诸多山水。'
+      },
+      {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/d5/abe8a48a6ef8bf.jpg_r_640x214_7c41cc81.jpg',
+        title: '武汉必打卡',
+        desc: '到武汉感受神奇的热干面之都。'
+      },
+      {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/6d/5afbf529076ef6.jpg_r_640x214_7414b151.jpg',
+        title: '三峡人家',
+        desc: '感受长江的水三峡的情怀，自古至今，无数骚人墨客浓墨重彩的描绘三峡，赶紧领略三峡的壮美吧！'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .recommend-title
+    margin-top: .2rem
+    line-height:.8rem
+    background: #eee
+    text-indent: .2rem
+    text-align: left
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.4375%
+  .item-img
+    width:100%
+  .item-info
+    padding: .14rem .2rem .2rem .2rem;
+    text-align:left
+    .item-title
+      line-height:.54rem
+      font-size: .28rem;
+      ellipsis()
+    .item-desc
+      color: #616161;
+      font-size: .24rem;
+      ellipsis()
+</style>
