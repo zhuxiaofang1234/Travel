fd7f20380a4bef0ff0720287e5f90b6fb345cf24
zhu <819508408@qq.com>
2019-11-18 22:17:57 +0800

    提交画廊组件

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,66 @@
+<template>
+ <div class="container" @click="handleGallaryClick">
+   <div class="wrapper">
+     <swiper :options="swiperOption">
+       <!-- slides -->
+       <swiper-slide v-for="(item , index) of imgs" :key="index">
+         <img class="swiper-img" :src="item"/>
+       </swiper-slide>
+       <div class="swiper-pagination"  slot="pagination"></div><!--实际是swiper组件里的内容，不是当前组件-->
+     </swiper>
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: 'swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display:flex
+    flex-direction:column
+    justify-content:center
+    z-index:99
+    position:fixed
+    width:100%
+    top:0
+    bottom:0
+    background: #000
+    .wrapper
+      width: 100%
+      height:0;
+      padding-bottom:100%
+      .swiper-img
+        width:100%
+      .swiper-pagination
+        color: #fff
+        bottom:-1rem
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1911/58/58502170165daf88a3.img.jpg_600x330_94e7d9e6.jpg"/>
+      <div class="banner-info">
+        <div class="banner-title">武汉欢乐谷(AAAA景区)</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe632;</span>39</div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs"
+                    v-show="showGallary"
+                    @close="handleGallaryClose"
+    ></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgs: [
+        'http://img1.qunarzz.com/sight/p0/1805/af/af648beb7239c330a3.img.png_r_800x800_97675cd8.png',
+        'http://img1.qunarzz.com/sight/p0/1904/47/479c814f9938a9b9a3.img.jpg_r_800x800_d36657c0.jpg',
+        'http://img1.qunarzz.com/sight/p0/1807/49/49f59108ed03bc63a3.img.png_r_800x800_4d8d7595.png',
+        'http://img1.qunarzz.com/sight/p0/1807/1a/1a4ab5ff1ab3d2dda3.img.png_r_800x800_d487628c.png',
+        'http://img1.qunarzz.com/sight/p0/1807/31/31b3c9c82847318aa3.img.png_r_800x800_0837fb46.png',
+        'http://img1.qunarzz.com/sight/p0/1806/74/74f2fd3a14231b58a3.img.jpg_r_800x800_4dc1fa06.jpg'
+      ]
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position:relative
+    overflow: hidden
+    height: 0
+    padding-bottom: 55%
+    .banner-img
+      width:100%;
+    .banner-info
+      display:flex
+      position: absolute
+      left:0
+      right:0
+      bottom:0
+      line-height: .6rem
+      color: #fff
+      text-align: left
+      .banner-title
+        flex:1
+        padding:0 .2rem
+        font-size: .32rem
+      .banner-number
+        line-height: .32rem
+        margin-top:.14rem
+        padding:0 .4rem
+        height:.32rem
+        border-radius:.2rem
+        background :rgba(0,0,0,.8)
+        font-size: .24rem
+        .banner-icon
+          font-size: .24rem
+</style>
